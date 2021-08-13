45f2c63576f6fdc6018344881b0776b1e2a4b3df
Jaxon Bennick <ljbennick5@gmail.com>
2021-08-13 10:47:47 -0600

    python analysis

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt")  # opening the file (um-server-01.txt)
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file):  # defining function
+    for line in log_file:  # finding each line in the file
+        line = line.rstrip()  # splitting each line into arrays
+        day = line[0:3]  # finding all days values
+        if day == "Tue":  # setting a condition
+            print(line)  # print those lines
 
 
-sales_reports(log_file)
+sales_reports(log_file)  # running that function on the file
